3189adb8c3858e2a72d86cf4c9958cd9a84ce8e1
GITSangWoo <dogcenta@gmail.com>
2024-08-06 14:44:42 +0900

    last fix

--- src/load_package/load.py
+++ src/load_package/load.py
@@ -4,26 +4,33 @@ import os
 import pandas as pd
 
 # 데이터 추가 전처리
-def col_type(load_dt,path):
-    #read data from transform / 경로 monthly 추가 변경 
-    df=pd.read_parquet(f"{path}/load_dt={load_dt}")
-    print("flag0: start data")
-    df.info()
-    #change data type and format
+def best():
+    df=pd.read_parquet("~/code/team_repo/transform_path")
     col=['rankInten','salesInten','salesChange']
     for i in col:
         df[i]=pd.to_numeric(df[i])
-    df['openDt'] = pd.to_datetime(df['openDt'])
+
+    df['openDt'] = pd.to_datetime(df['openDt'],errors='coerce')
     df['openDt'] = df['openDt'].dt.strftime('%Y%m%d')
     df['load_dt'] = df['load_dt'].astype(str)
     
-    print("flag1 : 데이터 타입, 형식  변했는지 확인:")
-    print(df.info())
-    print(df['load_dt'].head(3)) 
-    # 달 박스오피스 1위 출력 
     grouped = df.groupby('movieNm').agg({
-    'audiCnt': 'sum',
-    'openDt': 'max',
-    'load_dt' :'max'
+        'audiCnt': 'sum',
+        'openDt': 'max',
+        'load_dt' :'max'
     }).reset_index()
-    grouped.sort_values('audiCnt',ascending=False) 
+
+    grsort=grouped.sort_values('audiCnt',ascending=False)
+    grsort
+
+def special_dat():
+    day=['20230101','20230121','20230122','20230123','20230124','20230301','20230505','20230527','20230529','20230606','20230815','20230928','20230929','20230930','20231003','20231009','20231225']
+    special_day = df[df['load_dt'].isin(day)]
+    special_day.groupby('movieNm').agg({
+        'rank' : 'max',
+        'rankInten' : 'max',
+        'audiCnt': 'sum',
+        'openDt': 'max',
+        'load_dt' :'max'
+    }).sort_values(['load_dt','audiCnt'], ascending = False)
+    
